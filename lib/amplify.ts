92475fb3544f283bb8f0ef1fc48b86cd16679bd2
varun <varunvenkateshsarita@gmail.com>
2026-05-14 22:24:39 +0530

    fix: use dynamic amplifyconfiguration.json instead of hardcoded backend IDs

--- lib/amplify.ts
+++ lib/amplify.ts
@@ -1,20 +1,4 @@
 import { Amplify } from 'aws-amplify';
+import config from '../src/amplifyconfiguration.json';
 
-const awsConfig = {
-  Auth: {
-    Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID || 'eu-north-1_i9uKFiQnm',
-      userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID || '5j3372u54luccc6p178n1on5c1',
-      region: process.env.NEXT_PUBLIC_AWS_REGION || 'eu-north-1'
-    }
-  },
-   API: {
-    GraphQL: {
-      endpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT || 'https://x25vguh7tjgmngrnntzpdaje3i.appsync-api.eu-north-1.amazonaws.com/graphql',
-      region: process.env.NEXT_PUBLIC_AWS_REGION || 'eu-north-1',
-      defaultAuthMode: 'userPool' as const
-    }
-  }
-};
-
-Amplify.configure(awsConfig);
+Amplify.configure(config);
